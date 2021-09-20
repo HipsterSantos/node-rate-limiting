f18fb4b0e2776de93a2123320c0b71d3c676083f
hipster <santoscampos269@gmail.com>
2021-09-20 02:21:48 +0100

    data source added

---
+++ data.js
@@ -0,0 +1,31 @@
+{
+    "data": [
+        {
+            id: 1,
+            author: "Lilian",
+            title: "Stock market",
+            body: "Post 1",
+          },
+        
+          {
+            id: 2,
+            author: "Tom",
+            title: "Covid 19",
+            body: "Post 2",
+          },
+        
+          {
+            id: 3,
+            author: "Vincent",
+            title: "Django APIs",
+            body: "Post 3",
+          },
+        
+          {
+            id: 4,
+            author: "Cindy",
+            title: "Node.js Streams",
+            body: "Post 4",
+          }
+    ]
+}
